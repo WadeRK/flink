f787941c3f18ad18330b6e9b70853df885cfa810
WadeRK <86435212+WadeRK@users.noreply.github.com>
2025-03-20 06:22:24 +0200

    Update atrens.js

--- atrens.js
+++ atrens.js
@@ -43,10 +43,10 @@ let url = sessionStorage.getItem('url');
             } else {
                 window.location.href = 'https://threeholdings--c.vf.force.com/apex/MGAClientFileStatusChangePage?id=' + id + '&status=Renewed';
             }
-            localStorage.setItem('timetorenew', 1)
+            sessionStorage.setItem('timetorenew', 1)
         };
         input.click();
-    } else if (window.location.href.includes('MGAClientFileStatusChangePage') && localStorage.getItem('timetorenew') == 1) {
+    } else if (window.location.href.includes('MGAClientFileStatusChangePage') && sessionStorage.getItem('timetorenew') == 1) {
         alert(timetorenew);
     } else if (document.title.includes('Client file:') && sessionStorage.getItem('timetoamendaddress') === 'true') {
         console.log('timetoamendaddress before navigation:', sessionStorage.getItem('timetoamendaddress'));
